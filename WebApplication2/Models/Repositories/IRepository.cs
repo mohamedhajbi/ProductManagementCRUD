c835169f3fe8b6adbb1dfc67452e90aa49aa9b55
unknown <hajbi662@gmail.com>
2024-10-01 20:36:59 +0100

    CRUD

---
+++ WebApplication2/Models/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models.Repositories
+{
+    public interface IRepository<T>
+    {
+        T Get(int Id);
+        IEnumerable<T> GetAll();
+        T Add(T t);
+        T Update(T t);
+        T Delete(int Id);
+        IEnumerable<T> FindByDesignation(string designation);
+    }
+}
---
+++ WebApplication2/Models/Repositories/SqlProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Models.Repositories
+{
+    public class SqlProductRepository : IRepository<Product>
+    {
+        private readonly AppDbContext context;
+        public SqlProductRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public Product Add(Product P)
+        {
+            context.Products.Add(P);
+            context.SaveChanges();
+            return P;
+        }
+        public Product Delete(int Id)
+        {
+            Product P = context.Products.Find(Id);
+            if (P != null)
+            {
+                context.Products.Remove(P);
+                context.SaveChanges();
+            }
+            return P;
+        }
+        public IEnumerable<Product> GetAll()
+        {
+            return context.Products;
+        }
+        public Product Get(int Id)
+
+        {
+            return context.Products.Find(Id);
+        }
+        public Product Update(Product P)
+        {
+            var Product =
+            context.Products.Attach(P);
+            Product.State = EntityState.Modified;
+            context.SaveChanges();
+            return P;
+        }
+        public IEnumerable<Product> FindByDesignation(string designation)
+        {
+            return context.Products
+                          .AsEnumerable()  // Forcer l'évaluation côté client
+                          .Where(p => p.Désignation.Contains(designation, StringComparison.OrdinalIgnoreCase))
+                          .ToList();
+        }
+
+    }
+}
+    
